2117084fb0b2e3f0667a6e771c8ab9f47128caaa
Luke Imhoff <luke_imhoff@rapid7.com>
2014-05-08 13:09:26 -0500

    Simplecov
    MSP-9923

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in metasploit-version.gemspec
 gemspec
+
+group :test do
+  # code coverate for specs
+  gem 'simplecov', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+# require before 'metasploit/version' so coverage is shown for files required by 'metasploit/version'
+require 'simplecov'
+
 require 'metasploit/version'
 
 Dir[Metasploit::Version.root.join('spec', 'support', '**', '*.rb')].each do |f|
