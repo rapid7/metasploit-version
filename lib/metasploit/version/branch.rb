0439308562df0724da210945efde426434261c3e
Luke Imhoff <luke_imhoff@rapid7.com>
2014-09-11 09:44:15 -0500

    Add missing docs
    MSP-9923
    
    PullReview fix.

--- lib/metasploit/version/branch.rb
+++ lib/metasploit/version/branch.rb
@@ -2,11 +2,31 @@ require 'metasploit/version'
 
 # Regular expressions for parsing the branch name into its component parts
 module Metasploit::Version::Branch
+  #
+  # CONSTANTS
+  #
+
+  # Regular expression that matches, but does not capture an optional prefix of 'ref/remotes' for when a branch name is
+  # fully qualified on Jenkins.
   JENKINS_PREFIX_REGEXP = %r{(?:ref/remotes/)?}
+
+  # Matches runs of alphanumeric characters that are valid in prerelease segments.  Prerelease segments must be
+  # separated by `.` or `-`.
   PRERELEASE_SEGMENT_REGEXP = /[0-9a-zA-Z]+/
-  # Version pattern allowed by Rubygems for pre-release (i.e. summary in BRANCH_REGEXP)
+
+  # Version pattern allowed by Rubygems for pre-release: runs of alphanumeric characters separated by `.` or `-`.
+  # Group is captured to `:prerelease` name.
   PRERELEASE_REGEXP = /(?<prerelease>#{PRERELEASE_SEGMENT_REGEXP}([-.]#{PRERELEASE_SEGMENT_REGEXP})*)/
 
+  # Regular expression that matches exactly a staging branch.  It may
+  # {JENKINS_PREFIX_REGEXP optionally start with `ref/remotes`}, followed by a type of `staging`, no story ID, and a
+  # {PRERELEASE_REGEXP pre-release name}.
+  #
+  # @example Staging Branch
+  #   'staging/long-running'
+  #
+  # @example Staging Branch on Jenkins
+  #   'ref/remotes/origin/staging/long-running'
   STAGING_REGEXP = %r{
     \A
     #{JENKINS_PREFIX_REGEXP}
@@ -15,6 +35,29 @@ module Metasploit::Version::Branch
     #{PRERELEASE_REGEXP}
     \z
   }x
+
+  # Regular expression that matches exactly a chore, feature or bug branch.  It may
+  # {JENKINS_PREFIX_REGEXP optionally start with `ref/remotes`}, followed by a type of `staging`, story ID, and a
+  # {PRERELEASE_REGEXP pre-release name}.
+  #
+  # @example Bug Branch
+  #   'bug/MSP-666/nasty'
+  #
+  # @example Bug Branch on Jenkins
+  #   'ref/remotes/origin/bug/MSP-666/nasty'
+  #
+  # @example Chore Branch
+  #   'chore/MSP-1234/recurring'
+  #
+  # @example Chore Branch on Jenkins
+  #   'ref/remotes/origin/chore/MSP-1234/recurring'
+  #
+  # @example Feature Branch
+  #   'feature/MSP-31337/cool'
+  #
+  # @example Feature Branch on Jenkins
+  #   'ref/remotes/origin/feature/MSP-31337/cool'
+  #
   STORY_REGEXP = %r{
     \A
     #{JENKINS_PREFIX_REGEXP}
@@ -25,7 +68,20 @@ module Metasploit::Version::Branch
     #{PRERELEASE_REGEXP}
     \z
   }x
+
+  # Regular expression that matches separator used between {PRERELEASE_SEGMENT_REGEXP prerelease segments} for gem
+  # versions and tag versions.
   VERSION_PRERELEASE_SEGMENT_SEPARATOR_REGEXP = /\.pre\./
+
+  # Regular expression that exactly matches a release or pre-release version tag prefixed with `v` and followed by
+  # the major, minor, and patch numbers separated by '.' with an optional prerelease version suffix.
+  #
+  # @example Releease Tag
+  #   'v1.2.3'
+  #
+  # @example Prerelease Tag
+  #   'v1.2.3.pre.cool'
+  #
   VERSION_REGEXP = %r{
     \A
     v(?<major>\d+)
