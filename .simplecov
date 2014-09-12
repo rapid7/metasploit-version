64f61cd0e889028608dcb354f81dbd6969b5d0b2
Cucumber <cucumber@example.com>
2014-09-12 08:33:15 -0500

    Use coveralls and codeclimate report for cucumber
    MSP-9923
    
    Move the CodeClimate and Coveralls reporter setup from
    `spec/spec_helper.rb` to `.simplecov` so that they are setup for both
    `rake spec` and `rake cucumber` on travis-ci.

--- .simplecov
+++ .simplecov
@@ -40,3 +40,21 @@ SimpleCov.configure do
 
   add_group 'Specs', 'spec'
 end
+
+require 'codeclimate-test-reporter'
+require 'coveralls'
+
+if ENV['TRAVIS'] == 'true'
+  formatters = []
+
+  # don't use `CodeClimate::TestReporter.start` as it will overwrite some .simplecov settings
+  if CodeClimate::TestReporter.run?
+    formatters << CodeClimate::TestReporter::Formatter
+  end
+
+  formatters << Coveralls::SimpleCov::Formatter
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    *formatters
+  ]
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,23 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 # require before 'metasploit/version' so coverage is shown for files required by 'metasploit/version'
 require 'simplecov'
-require 'codeclimate-test-reporter'
-require 'coveralls'
-
-if ENV['TRAVIS'] == 'true'
-  formatters = []
-
-  # don't use `CodeClimate::TestReporter.start` as it will overwrite some .simplecov settings
-  if CodeClimate::TestReporter.run?
-    formatters << CodeClimate::TestReporter::Formatter
-  end
-
-  formatters << Coveralls::SimpleCov::Formatter
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    *formatters
-  ]
-end
 
 require 'metasploit/version'
 
