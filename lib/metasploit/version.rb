06c60a141629f2bae257d708f5f01887a553a217
Luke Imhoff <luke_imhoff@rapid7.com>
2014-09-15 09:45:17 -0500

    Remove unnecessary `root`
    MSP-11354
    
    Root of gems can be derived using Gem::Specification.

--- lib/metasploit/version.rb
+++ lib/metasploit/version.rb
@@ -15,12 +15,5 @@ module Metasploit
   # Namespace for this gem.
   module Version
     autoload :Branch, 'metasploit/version/branch'
-
-    # The root of the `metasploit-version` gem file system.
-    #
-    # @return [Pathname]
-    def self.root
-      @root ||= Pathname.new(__FILE__).parent.parent.parent
-    end
   end
 end
