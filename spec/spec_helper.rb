dd721b544b2fa69d42c093f6fbe796c7b93f19ff
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-06 09:32:20 -0500

    Disable simplecov for JRuby and rbx for specs
    MSP-11103
    
    Had to disable for cucumber, so disable for specs too since coverage is
    only complete with both cucumber and specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+if RUBY_ENGINE == 'ruby'
 # require before 'metasploit/version' so coverage is shown for files required by 'metasploit/version'
-require 'simplecov'
+  require 'simplecov'
+end
 
 require 'metasploit/version'
 
